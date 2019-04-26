4e26fd5184acf714dc7ef4e16a18d451b96e6cbf
qZhang88 <qzhang8806@gmail.com>
2019-04-26 20:59:44 +0800

    update

--- 124. Binary Tree Maximum Path Sum/solution.h
+++ 124. Binary Tree Maximum Path Sum/solution.h
@@ -1,26 +1,31 @@
 #include <cstddef>
 #include <algorithm>
+#include <limits.h>
+
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {};
 };
 
-class Solution {
-    int maxPathSum(TreeNode *root, int &maxSum) {
-        if (!root) return 0;
-        int leftMax = std::max(0, maxPathSum(root->left, maxSum));
-        int rightMax = std::max(0, maxPathSum(root->right, maxSum));
-        maxSum = std::max(maxSum, leftMax+rightMax+root->val);
-        return root->val + std::max(leftMax, rightMax);
-    }
 
+class Solution {
 public:
-    int maxPathSum(TreeNode *root) {
-        int maxSum = INT_MIN;
-        maxPathSum(root, maxSum);
-        return maxSum;
-    }
+	int maxPathSum(TreeNode* root){
+		ans = INT_MIN;
+		arrowSum(root);
+		return ans;
+	}
+
+private:
+	int ans;
+	int arrowSum(TreeNode * node) {
+		if (!node) return 0;
+		int arrowLeft = arrowSum(node->left);
+		int arrowRight = arrowSum(node->right);
+		ans = std::max(ans, arrowLeft + arrowRight + node->val);
+		return std::max(0, std::max(arrowLeft, arrowRight) + node->val);
+	}
 };
